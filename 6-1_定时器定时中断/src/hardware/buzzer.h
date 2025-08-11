8b48f26dd5378ea439675158fcdb4da214c3497f
x <x@x.com>
2025-08-12 00:26:26 +0800

    完成6-1

---
+++ 6-1_定时器定时中断/src/hardware/buzzer.h
@@ -0,0 +1,8 @@
+#if !defined(__BUZZER_H)
+#define __BUZZER_H
+
+void buzzer_init();
+void buzzer_on();
+void buzzer_off();
+
+#endif // __BUZZER_H
---
+++ 6-1_定时器定时中断/src/main.c
@@ -0,0 +1,25 @@
+#include <stm32f10x.h>
+#include "hardware/OLED.h"
+#include "system/Timer.h"
+
+unsigned int timer_counter = 0;
+
+int main(int argc, char const *argv[])
+{
+    OLED_Init();
+    timer_init();
+    OLED_ShowString(1, 1, "Number:");
+    while (1) {
+        OLED_ShowNum(1, 8, timer_counter, 6);
+    }
+
+    return 0;
+}
+
+void TIM2_IRQHandler()
+{
+    if (TIM_GetITStatus(TIM2, TIM_IT_Update) == SET) {
+        timer_counter++;
+        TIM_ClearITPendingBit(TIM2, TIM_IT_Update);
+    }
+}
