9438250f6bfe570654a5faf30cebda24c96ec9a9
x <x@x.com>
2025-08-27 23:16:46 +0800

    完成6-8

---
+++ 6-8_编码器接口测速/src/main.c
@@ -0,0 +1,34 @@
+#include <stm32f10x.h>
+#include "hardware/OLED.h"
+#include "system/Timer.h"
+#include "hardware/encoder_speed.h"
+
+int last_position = 0;
+int speed         = 0;
+
+int main(int argc, char const *argv[])
+{
+    OLED_Init();
+    timer_init();
+    encoder_init();
+    OLED_ShowString(1, 1, "Position:");
+    OLED_ShowString(2, 1, "Speed:");
+    while (1) {
+        OLED_ShowSignedNum(1, 10, encoder_get_position(), 5);
+        OLED_ShowSignedNum(2, 10, speed, 5);
+    }
+
+    return 0;
+}
+
+void TIM2_IRQHandler()
+{
+    int current_position;
+    // 0.5s
+    if (TIM_GetITStatus(TIM2, TIM_IT_Update) == SET) {
+        current_position = encoder_get_position();
+        speed            = (current_position - last_position) * 2;
+        last_position    = current_position;
+        TIM_ClearITPendingBit(TIM2, TIM_IT_Update);
+    }
+}
